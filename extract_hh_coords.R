48199537f5f839da25bbe00a10ec9570037f1092
Alex Zvoleff <azvoleff@mail.sdsu.edu>
2011-04-22 16:54:39 -0700

    Use EPSG code for UTM30 projection.

--- extract_hh_coords.R
+++ extract_hh_coords.R
@@ -8,7 +8,9 @@ DPI <- 300
 WIDTH <- 8.33
 HEIGHT <- 5.53
 
-load("whsa_ii_data050510.Rdata")
+#load("whsa_ii_data050510.Rdata")
+#load("R:/Data/Ghana/20101206/whsa_ii_data050510.Rdata")
+load("F:/Data/Ghana/20101206/whsa_ii_data050510.Rdata")
 
 hh <- with(whsa2data050510, data.frame(HHID, x=longitude, y=latitude))
 # Throw out household with longitude < .3 (too far from the rest to be a real 
@@ -17,7 +19,8 @@ hh <- hh[!(hh$x < -.3),]
 
 coordinates(hh) <- c("x", "y")
 proj4string(hh) <- CRS("+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs")
-hh.utm <- spTransform(hh, CRS("+proj=utm +zone=30 +ellps=WGS84 +units=m +no_defs"))
+#hh.utm <- spTransform(hh, CRS("+proj=utm +zone=30 +ellps=WGS84 +units=m +no_defs"))
+hh.utm <- spTransform(hh, CRS("+init=epsg:32630"))
 
 writeOGR(hh.utm, "WHSA_hh_UTM30.shp", "WHSA_hh", "ESRI Shapefile")
 
