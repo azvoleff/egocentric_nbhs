cd1b8a3d0810da91c8df921ef94dc33f2cde656c
Alex Zvoleff <azvoleff@mail.sdsu.edu>
2011-04-22 16:54:59 -0700

    Continue work on coding pixel extraction.

--- extract_hh_pixels.py
+++ extract_hh_pixels.py
@@ -14,12 +14,15 @@ from osgeo import gdal
 # window size is the number of pixels included on each side of the center 
 # pixel. So a window_size of one gives a square window that is 3 pixels per 
 # size. For a 12 meter window on QuickBird multispectral data, use a window 
-# size of 2 (giving a window that is 5 pixels per side).
-window_size = 0 # Single 2.4m pixel
-window_size = 2 # Window that is 12 meters per side
+# size of 2 (giving a window that is 5 pixels per side). The window size 
+# determines the maximum buffer radius that can be considered.
+#window_size = 0 # Single 2.4m pixel
+#window_size = 2 # Window that is 12 meters per side
+window_size = 250 # Window that is 12 meters per side
 
-ds = gdal.Open("/media/Orange_Data/Data/Imagery/Ghana/VIS/Ghana_VIS_masked_geotiff.tif")
+#ds = gdal.Open("/media/Orange_Data/Data/Imagery/Ghana/VIS/Ghana_VIS_masked_geotiff.tif")
 #ds = gdal.Open("R:/Data/Imagery/Ghana/VIS/Ghana_VIS_masked_geotiff.tif")
+ds = gdal.Open("F:/Data/Imagery/Ghana/VIS/Ghana_VIS_masked_geotiff.tif")
 
 width = ds.RasterXSize
 height = ds.RasterYSize
@@ -41,13 +44,21 @@ def convert_to_img_coords(x, y):
     img_y =  int(np.floor((y - maxy)/y_res))
     return img_x, img_y
 
-hh_coords = np.recfromcsv("WHSAII_household_locations_UTM30.csv")
+# Calculate the distance matrix, storing in a matrix the distance of each cell 
+# from the center point of the matrix (where the matrix is a square matrix with 
+# (window_size*2)*1 rows and columns). The distances in the matrix are 
+# expressed in number of pixels - not directly in distance.
 
-data = []
+hh_coords = np.recfromcsv("WHSA_hh_UTM30.csv")
+# Extract a window surrounding each household pixel, where the window has an 
+# edge length (in pixels) of: (window_size*2)*1.
 # The HHID column will be prepended to the output columns to identify the 
 # household ID of each data row, and allow merging of the spectral data with 
 # the household-level data.
+data = []
 HHID = []
+window = ds.ReadAsArray(img_x-window_size, img_y-window_size,
+    (window_size*2)+1, (window_size*2)+1)
 for x, y in zip(hh_coords.x, hh_coords.y):
     img_x, img_y = convert_to_img_coords(x, y)
     window = ds.ReadAsArray(img_x-window_size, img_y-window_size,
@@ -55,8 +66,6 @@ for x, y in zip(hh_coords.x, hh_coords.y):
     data.append(window)
 data = np.array(data, dtype='float32')
 ds.destroy()
-
-sys.exit()
 
 # Calculate range, mean, and standard deviation of each window for each band 
 # (or vegetation index).
@@ -68,29 +77,3 @@ window_width = (window_size*2) + 1
 means = sums/(window_width**2)
 means_reshaped = means.reshape(means.shape[0],means.shape[1],1,1)
 stds = np.sqrt(np.sum(np.sum((data-means_reshaped)**2,3),2)/(window_width**2))
-
-output_file = "spectral_results_%spixelwindow.csv"%(window_size*2+1)
-try:
-    output_file_obj = open(output_file, "w")
-except:
-    raise IOError("Unable to open %s"%output_file)
-# Make and write the CSV file header:
-header_prefixes = ["range_", "mean_", "std_"]
-header_suffixes = ["B1", "B2", "B3", "B4", "NDVI", "MSAVI"]
-header = "HHID"
-for prefix in header_prefixes:
-    for suffix in header_suffixes:
-        header = header + ", " + prefix + suffix
-header = header + "\n"
-output_file_obj.write(header)
-
-HHID = np.array(hh_coords.HHID).reshape(np.shape(data)[0],1)
-output_data = np.hstack([HHID, range, means, stds])
-# First convert them all to strings to get the proper precision
-for row in output_data:
-    # Start with an integer as HHID field needs no conversion
-    output_file_obj.write("%s"%row[0])
-    for item in row[1:]:
-        output_file_obj.write(", %.10f"%float(item))
-    output_file_obj.write("\n")
-output_file_obj.close()
