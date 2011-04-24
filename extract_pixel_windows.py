f922789472edcd5fafecc6d61607d6533ce1b25d
Alex Zvoleff <azvoleff@mail.sdsu.edu>
2011-04-23 18:22:47 -0700

    Fine-tune window extraction code.

--- extract_pixel_windows.py
+++ extract_pixel_windows.py
@@ -3,7 +3,6 @@
 # Used to extract a window of pixel around a each point in a set of points.
 # The image must be in a projected coordinate system.
 
-import sys
 import os
 
 import numpy as np
@@ -18,78 +17,89 @@ from osgeo import gdal
 # determines the maximum buffer radius that can be considered.
 #window_size = 0 # Single 2.4m pixel
 #window_size = 2 # Window that is 12 meters per side
-window_size = 50 # Window that is 12 meters per side
+window_size = 25 # Window that is 12 meters per side
 window_width = (window_size*2) + 1
 # Number of classes in the image. If the classes are not denoted by sequention 
 # numbers, the code will need to be modified where the 'classes' variable is 
 # setup towards the end of the code.
 num_classes = 4 
 
-data_filename = 'VIS_%spixels_windows.npy'%window_size
-results_filename = 'VIS_%spixels_results.npy'%window_size
+data_filename = 'VIS_%spixels_windows.npz'%window_size
 
 if os.path.exists(data_filename):
-    raise IOError("File "%s" already exists. Must delete file to regenerate.")
-else:
-    print("***Loading household coordinate data...")
-    #ds = gdal.Open("/media/Orange_Data/Data/Imagery/Ghana/VIS/Ghana_VIS_masked_geotiff.tif")
-    ds = gdal.Open("/media/G-Tech_Data/Data/Imagery/Ghana/VIS/Ghana_VIS_masked_geotiff.tif")
-    #ds = gdal.Open("R:/Data/Imagery/Ghana/VIS/Ghana_VIS_masked_geotiff.tif")
-    #ds = gdal.Open("F:/Data/Imagery/Ghana/VIS/Ghana_VIS_masked_geotiff.tif")
-    cols = ds.RasterXSize
-    rows = ds.RasterYSize
-    gt = ds.GetGeoTransform()
-    # Format of ds.GetGeoTransform is:
-    # (upper left x, x-size, rotation1, upper left y, rotation2, y-size)
-    # upper left is the origin for GDAL
-    origin_x = gt[0]
-    origin_y = gt[3]
-    pixel_width = gt[1]
-    pixel_height = gt[5]
+    raise IOError('File "%s" already exists. Must delete file to regenerate.'%data_filename)
 
-    def convert_to_img_coords(x, y):
-        img_x = int((x - origin_x)/pixel_width)
-        img_y = int((y - origin_y)/pixel_height)
-        return img_x, img_y
+print("***Loading household coordinate data...")
+#ds = gdal.Open("/media/Orange_Data/Data/Imagery/Ghana/VIS/Ghana_VIS_masked_geotiff.tif")
+ds = gdal.Open("/media/G-Tech_Data/Data/Imagery/Ghana/VIS/Ghana_VIS_masked_geotiff.tif")
+#ds = gdal.Open("R:/Data/Imagery/Ghana/VIS/Ghana_VIS_masked_geotiff.tif")
+#ds = gdal.Open("F:/Data/Imagery/Ghana/VIS/Ghana_VIS_masked_geotiff.tif")
+cols = ds.RasterXSize
+rows = ds.RasterYSize
+gt = ds.GetGeoTransform()
+# Format of ds.GetGeoTransform is:
+# (upper left x, x-size, rotation1, upper left y, rotation2, y-size)
+# upper left is the origin for GDAL
+origin_x = gt[0]
+origin_y = gt[3]
+pixel_width = gt[1]
+pixel_height = gt[5]
 
-    hh_coords = np.recfromcsv("WHSA_hh_UTM30.csv")
-    # TODO: Fix this. For now drop all hh where their coords are outside the raster 
-    # boundary, or where their coords are close enough to the raster boundary that 
-    # any part of a window_size sized window would fall outside the raster 
-    # boundary.
-    lower_right_x = origin_x + cols * pixel_width
-    lower_right_y = origin_y + rows * pixel_height
-    min_x = origin_x + window_size * pixel_width
-    max_x = lower_right_x - (window_size + 1) * pixel_width
-    max_y = origin_y - (window_size + 1) * np.abs(pixel_height)
-    min_y = lower_right_y + window_size * np.abs(pixel_height)
-    initial_shape = hh_coords.shape[0]
-    hh_coords = hh_coords[(hh_coords.x > min_x) & (hh_coords.x < max_x),]
-    hh_coords = hh_coords[(hh_coords.y > min_y) & (hh_coords.y < max_y),]
-    final_shape = hh_coords.shape[0]
-    print("Dropped %s households outside raster extent"%(initial_shape-final_shape))
+def convert_to_img_coords(x, y):
+    img_x = int((x - origin_x)/pixel_width)
+    img_y = int((y - origin_y)/pixel_height)
+    return img_x, img_y
 
-    # Extract a window surrounding each household pixel, where the window has an 
-    # edge length (in pixels) of: (window_size*2)*1.
-    # The HHID column will be prepended to the output columns to identify the 
-    # household ID of each data row, and allow merging of the spectral data with 
-    # the household-level data.
-    image = ds.ReadAsArray().transpose()
-    HHID = []
-    first_run = True
-    n=0
-    for x, y in zip(hh_coords.x, hh_coords.y):
-        print n,
-        n+=1
-        center_x, center_y = convert_to_img_coords(x, y)
-        # In the below lieves ul means "upper left", lr means "lower right"
-        ul_x, ul_y = center_x-window_size, center_y+window_size+1
-        lr_x, lr_y = center_x+window_size+1, center_y-window_size
-        box = image[ul_x:lr_x, lr_y:ul_y]
-        if first_run==True:
-            data = np.array(box, dtype='int8')
-            first_run = False
-        else:
-            data = np.dstack((data, box))
-    print("\nSaving data...")
-    np.savez(data_filename, data)
+hh_coords = np.recfromcsv("WHSA_hh_UTM30.csv")
+# TODO: Fix this. For now drop all hh where their coords are outside the raster 
+# boundary, or where their coords are close enough to the raster boundary that 
+# any part of a window_size sized window would fall outside the raster 
+# boundary.
+lower_right_x = origin_x + cols * pixel_width
+lower_right_y = origin_y + rows * pixel_height
+min_x = origin_x + window_size * pixel_width
+max_x = lower_right_x - (window_size + 1) * pixel_width
+max_y = origin_y - (window_size + 1) * np.abs(pixel_height)
+min_y = lower_right_y + window_size * np.abs(pixel_height)
+initial_shape = hh_coords.shape[0]
+hh_coords = hh_coords[(hh_coords.x > min_x) & (hh_coords.x < max_x),]
+hh_coords = hh_coords[(hh_coords.y > min_y) & (hh_coords.y < max_y),]
+final_shape = hh_coords.shape[0]
+print("\tDropped %s households outside raster extent."%(initial_shape-final_shape))
+
+print("***Extracting windows...")
+# Extract a window surrounding each household pixel, where the window has an 
+# edge length (in pixels) of: (window_size*2)*1.
+image = ds.ReadAsArray().transpose()
+data = np.zeros((window_width, window_width, len(hh_coords.x)), dtype='int8')
+for hh_num in xrange(0, len(hh_coords.x)):
+    x = hh_coords.x[hh_num]
+    y = hh_coords.y[hh_num]
+    center_x, center_y = convert_to_img_coords(x, y)
+    # In the below lieves ul means "upper left", lr means "lower right"
+    ul_x, ul_y = center_x-window_size, center_y+window_size+1
+    lr_x, lr_y = center_x+window_size+1, center_y-window_size
+    box = image[ul_x:lr_x, lr_y:ul_y]
+    data[:,:,hh_num] = box
+
+print("\n***Calculating distance matrix...")
+# Calculate the distance matrix, storing in a matrix the distance of each cell 
+# from the center point of the matrix (where the matrix is a square matrix with 
+# (window_size*2)*1 rows and columns). The distances in the matrix are 
+# expressed directly in distance by multiplying by the resolution of the raster 
+# (this converts the distances from being expressed in number of pixels to 
+# number of meters).
+x_dist = np.arange(-window_size, window_size+1, 1) * np.ones((window_width, 1))
+# Convert to meters
+x_dist = x_dist * np.abs(pixel_width)
+y_dist = x_dist.transpose()*np.abs(pixel_height)
+# Use the distance formula (where the center point has a (x,y) location of 
+# (0,0):
+dists = np.sqrt(x_dist**2+y_dist**2)
+print dists.shape
+print np.tile(dists, (data.shape[2], 1, 1)).shape
+dists = np.tile(dists, (data.shape[2], 1, 1))
+print dists.shape.transpose()
+
+print("***Saving data...")
+np.savez(data_filename, data=data, window_size=window_size, dists=dists)
