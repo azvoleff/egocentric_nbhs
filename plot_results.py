6ca6f6adbbb3e4e8b5102be83958e043cf1924b9
Alex Zvoleff <azvoleff@mail.sdsu.edu>
2011-04-25 00:24:52 -0700

    Finish plotting code.

--- plot_results.py
+++ plot_results.py
@@ -1,21 +1,34 @@
 import matplotlib
 from mpl_toolkits.mplot3d import Axes3D
-from matplotlib import cm
 import matplotlib.pyplot as plt
 import numpy as np
 
-for index in xrange(0:len(max_dists)):
-    max_dist
+window_size = 50
+
+results_filename = 'VIS_%spixels_results.npz'%window_size
+results_array = np.load(results_filename)
+results = results_array['results']
+max_dists = max_dists_array['max_dists']
+window_size = window_size_array['window_size']
+
+for max_dist_index in xrange(0:len(max_dists)):
     fig = plt.figure(figsize=(12, 8), facecolor='w', edgecolor='k', 
             subplotpars=matplotlib.figure.SubplotParams(left=.05, right=.95, 
             bottom=.05, top=.95))
     ax = fig.gca(projection='3d')
-    S1_data = np.genfromtxt(model + datafile_suffix, delimiter=",")
-    surf = ax.plot(results[1], Y_S1, Z_S1, rstride=1, cstride=1,
-            linewidth=0, antialiased=True, alpha=.9)
+
+    x = results[:, max_dist_index, 0]
+    y = results[:, max_dist_index, 1]
+    z = results[:, max_dist_index, 2]
+
+    points = ax.(x, y, z, rstride=1, cstride=1,
+            antialiased=True, alpha=.9)
+
     ax.set_xlabel("Vegetation (%)")
     ax.set_ylabel("Soil (%)")
     ax.set_zlabel("Impervious (%)")
+    max_dist = max_dists[max_dist_index]
+    ax.set_title(" %s Meter Neighborhood Radius"%max_dist)
     ax.set_xlim3d(0, 100)
     ax.set_ylim3d(0, 100)
     ax.set_zlim3d(0, 100)
