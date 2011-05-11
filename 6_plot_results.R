de98c3fa6a9d7fa4bce3a8ae7d01aa722990e1fd
Alex Zvoleff <azvoleff@mail.sdsu.edu>
2011-05-11 15:39:26 -0700

    Finalize ternary plot plotting code.

--- 6_plot_results.R
+++ 6_plot_results.R
@@ -5,30 +5,49 @@
 
 require("plotrix")
 
-window_size <- 50
+min_radius <- 50
+max_radius <- 975
+dradius <- 25
 
-data_dir <- '/home/azvoleff/Data/Ghana/Ecocentric_NBH_Data/'
+radii <- seq(min_radius, max_radius, dradius)
 
-filename_base = paste(data_dir, 'VIS_', window_size, 'pixels_results_', sep="")
+classes <- c('NA', 'NONVEG', 'VEG')
+#col_prefix <- 'QB2002_NDVI_'
+col_prefix <- 'QB2010_NDVI_'
 
-maxdists_filename = paste(filename_base, 'maxdists.csv', sep="")
-max_dists = read.table(maxdists_filename)
+#classes <- c('VEG', 'SOIL', 'IMPERVIOUS')
+#col_prefix <- 'QB2002_OBIA_'
 
-image_filename = paste(filename_base, 'mixingdiagram.pdf', sep="")
+#data_dir <- '/home/azvoleff/Data/Ghana/Ecocentric_NBH_Data/'
+data_dir <- '/home/azvoleff/Data/Ghana/Ecocentric_NBH_Data/20110503_100-to-1000-by-25m/'
 
-pdf(file=image_filename)
-for (max_dist_index in 1:nrow(max_dists)) {
-    max_dist = max_dists[max_dist_index, 1]
-    data_filename = paste(filename_base, 'maxdist', max_dist, '.csv', sep="")
-    VIS_matrix = read.table(data_filename)
-    VIS_matrix[is.na(VIS_matrix)] <- 0
-    VIS_matrix[,1] = round(VIS_matrix[,1],5)
-    VIS_matrix[,2] = round(VIS_matrix[,2],5)
-    VIS_matrix[,3] = 100 - VIS_matrix[,1]- VIS_matrix[,2]
-    VIS_matrix[VIS_matrix<0] <- 0
+load(paste(data_dir, 'merged_buffer_results.Rdata', sep=""))
 
-    plot_title = paste(max_dist, "Meter Neighborhood Radius")
-    triax.plot(VIS_matrix, main=plot_title, show.grid=TRUE,
-            axis.labels=c("Vegetation", "Soil", "Impervious"))
+if (length(classes) != 3) {
+    stop("Length of classes MUST equal 3")
+}
+
+
+pdf(file=paste(data_dir, col_prefix, 'ternary_plot.pdf', sep=""))
+par(cex=1.4, oma=c(0, 0, .5, 0))
+for (radius in radii) {
+    col_names <- paste(col_prefix, radius, 'm_', classes, sep='')
+    col_nums <- c()
+    for (col_name in col_names) {
+        col_nums <- c(col_nums,  grep(col_name, names(merged_results)))
+    }
+    prop_matrix <- merged_results[col_nums]
+    prop_matrix[is.na(prop_matrix)] <- 0
+    prop_matrix[,1] <- round(prop_matrix[,1],5)
+    prop_matrix[,2] <- round(prop_matrix[,2],5)
+    prop_matrix[,3] <- 100 - prop_matrix[,1]- prop_matrix[,2]
+    prop_matrix[prop_matrix<0] <- 0
+
+    area_hectares = round((pi*radius**2)/10000,1)
+
+    plot_title <- paste(radius, "m radius (", area_hectares, " hectares)", sep="")
+    # NOTE: mar format: c(bottom, left, top, right)
+    triax.plot(prop_matrix, main=plot_title, sub="test", show.grid=TRUE,
+            axis.labels=classes, mar=c(4, 0, 1, 0))
 }
 dev.off()
