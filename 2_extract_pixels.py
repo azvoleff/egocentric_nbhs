c171f654049fc77d9a6098fd342b17724b6d02fb
Alex Zvoleff <azvoleff@mail.sdsu.edu>
2012-02-22 20:49:35 -0800

    Fix RasterXSize to RasterYSize for row number.

--- 2_extract_pixels.py
+++ 2_extract_pixels.py
@@ -9,19 +9,19 @@ import numpy as np
 from osgeo import gdal
 
 #data_dir = '/home/azvoleff/Data/Ghana/Egocentric_NBH_Data/'
-data_dir = 'G:/Data/Ghana/Egocentric_NBH_Data/'
+data_dir = 'M:/Data/Ghana/Egocentric_NBH_Data/'
 
 ###############################################################################
 # Accra VIS Imagery
-#ds = gdal.Open("G:/Data/Imagery/Ghana/Accra_VIS/Ghana_VIS_masked_geotiff.tif")
+#ds = gdal.Open("M:/Data/Imagery/Ghana/Accra_VIS/Ghana_VIS_masked_geotiff.tif")
 #image_export_filename = data_dir + 'VIS_image.npz'
 
 ###############################################################################
 # Accra NDVI images
-#ds = gdal.Open("G:/Data/Imagery/Ghana/Accra_NDVI/cloud_masked_qb02_ndvi_ge120_v02.tif")
-#image_export_filename = data_dir + 'Quickbird_2002_NDVI_thresholded.npz'
-ds = gdal.Open("G:/Data/Imagery/Ghana/Accra_NDVI/cloud_masked_qb10_ndvi_ge120_v02.tif")
-image_export_filename = data_dir + 'Quickbird_2010_NDVI_thresholded.npz'
+ds = gdal.Open("M:/Data/Imagery/Ghana/Accra_NDVI/cloud_masked_qb02_ndvi_ge120_v02.tif")
+image_export_filename = data_dir + 'Quickbird_2002_NDVI_thresholded.npz'
+#ds = gdal.Open("M:/Data/Imagery/Ghana/Accra_NDVI/cloud_masked_qb10_ndvi_ge120_v02.tif")
+#image_export_filename = data_dir + 'Quickbird_2010_NDVI_thresholded.npz'
 
 ###############################################################################
 # Image processing code (to save image as a numpy array) starts below.
@@ -37,4 +37,4 @@ image = ds.ReadAsArray()
 # upper left is the origin for GDAL
 np.savez(image_export_filename, image=image, origin_x=gt[0], origin_y=gt[3],
         pixel_width=gt[1], pixel_height=gt[5], cols=ds.RasterXSize,
-        rows=ds.RasterXSize)
+        rows=ds.RasterYSize)
