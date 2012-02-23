23312e03e8c48877f3ef432122a56f8f70b5ce63
Alex Zvoleff <azvoleff@mail.sdsu.edu>
2012-02-23 14:48:24 -0800

    Redo reformatting code.

--- 5_reformat_results.R
+++ 5_reformat_results.R
@@ -5,42 +5,38 @@
 
 #data_dir <- '/home/azvoleff/Data/Ghana/Egocentric_NBH_Data/'
 #data_dir <- '/home/azvoleff/Data/Ghana/Egocentric_NBH_Data/20110503_100-to-1000-by-25m/'
-data_dir <- 'G:/Data/Ghana/Egocentric_NBH_Data/'
+data_dir <- 'M:/Data/Ghana/Egocentric_NBH_Data/'
 
 window_size <- 425
 
-#load("/home/azvoleff/Data/Ghana/20101206/whsa_ii_data050510.Rdata")
-load("G:/Data/Ghana/WHSA/whsa_ii_data050510.Rdata")
+# Add back in the household with longitude < .3 (was too far from the rest to 
+# be a real datapoint so it was dropped).
+load("M:/Data/Ghana/WHSA/whsa_ii_data050510.Rdata")
+extra_hh <- whsa2data050510[whsa2data050510$longitude < -.3,]
+extra_hh <- data.frame(ROWID=as.numeric(rownames(extra_hh)))
 
-hh <- with(whsa2data050510, data.frame(HHID, x=longitude, y=latitude))
-# Bind the rownames so they can be used as unique IDs in the later merge.
-hh <- cbind(hh, ROWID=rownames(hh))
-# Add back in the household with longitude < .3 (too far from the rest to be a 
-# real datapoint).
-extra_household <- hh[hh$x < -.3,]
-extra_household$x <- NA
-extra_household$y <- NA
-
-VIS_2002_filename <- paste(data_dir, 'VIS_', window_size, 'pixels_results.csv', sep="")
+#VIS_2002_filename <- paste(data_dir, 'VIS_', window_size, 'pixels_results.Rdata', sep="")
 NDVI_2002_filename <- paste(data_dir, '2002NDVI_', window_size, 'pixels_results.csv', sep="")
 NDVI_2010_filename <- paste(data_dir, '2010NDVI_', window_size, 'pixels_results.csv', sep="")
 
-# Bind the rownames so they can be used as unique IDs in the later merge.
-VIS_2002 <- read.csv(VIS_2002_filename)
-VIS_2002 <- cbind(VIS_2002, ROWID=rownames(VIS_2002))
-
 NDVI_2002 <- read.csv(NDVI_2002_filename)
-NDVI_2002 <- cbind(NDVI_2002, ROWID=rownames(NDVI_2002))
-
+NDVI_2002 <- cbind(NDVI_2002, ROWID=as.numeric(rownames(NDVI_2002)))
 NDVI_2010 <- read.csv(NDVI_2010_filename)
-NDVI_2010 <- cbind(NDVI_2010, ROWID=rownames(NDVI_2010))
+NDVI_2010 <- cbind(NDVI_2010, ROWID=as.numeric(rownames(NDVI_2010)))
 
 # First merge the two NDVI datasets - the VIS dataset has NAs for many 
 # households as the VIS image does not cover the full extent of the WHSA study 
 # area.
-merged_results <- merge(NDVI_2002, NDVI_2010)
-merged_results <- merge(merged_results, VIS_2002, all.x=TRUE)
-merged_results <- merge(merged_results, extra_household, all=TRUE)
+# Drop  duplicate columns in NDVI_2010 and VIS_2002:
+NDVI_2010 <- NDVI_2010[!(names(NDVI_2010) %in% c("x", "y", "woman_id"))]
+#VIS_2002 <- VIS_2002[!(names(VIS_2002) %in% c("x", "y"))]
+
+merged_results <- merge(NDVI_2002, NDVI_2010, by=c("ROWID"), sort=TRUE)
+# Increment the ROWIDs in merged_results to account for the missing HH
+row_id_gt_extra_hh <- merged_results$ROWID >= extra_hh$ROWID
+merged_results$ROWID[row_id_gt_extra_hh] <- merged_results$ROWID[row_id_gt_extra_hh] + 1
+#merged_results <- merge(merged_results, VIS_2002, by=c("ROWID", "woman_id"), all.x=TRUE)
+merged_results <- merge(merged_results, extra_hh, by=c("ROWID"), all=TRUE)
 
 # There shouldn't be any NAs (not directly). Whenever nothing is known about a 
 # neighborhoods cover type in a particular buffer, the _NA column for that 
