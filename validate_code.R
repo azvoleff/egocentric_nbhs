ab634920dd88eaff464c39c1f390d1b93028272e
Alex Zvoleff <azvoleff@mail.sdsu.edu>
2012-03-15 16:14:29 -0700

    Add validation code.

---
+++ validate_code.R
@@ -0,0 +1,44 @@
+#!/usr/bin/python
+# Uses GDAL to convert a geotiff image to a numpy array for later processing to 
+# choose a window of pixels around each of the WHSA surveyed households.
+
+import os
+
+import numpy as np
+
+from osgeo import gdal
+from osgeo import ogr
+
+#data_dir = '/home/azvoleff/Data/Ghana/Egocentric_NBH_Data/'
+data_dir = 'M:/Data/Ghana/Egocentric_NBH_Data/'
+
+# Read the WHSA2 buffer
+ds = ogr.Open("M:/Data/Ghana/WHSA/whsa2_100m_buffer.shp")
+
+###############################################################################
+# Accra VIS Imagery
+ds = gdal.Open("M:/Data/Ghana/Imagery/Accra_VIS/Ghana_VIS_masked_geotiff.tif")
+image_export_filename = data_dir + 'VIS_image.npz'
+
+###############################################################################
+# Accra NDVI images
+#ds = gdal.Open("M:/Data/Ghana/Imagery/Accra_NDVI/cloud_masked_qb02_ndvi_ge120_v02.tif")
+#image_export_filename = data_dir + 'Quickbird_2002_NDVI_thresholded.npz'
+ds = gdal.Open("M:/Data/Ghana/Imagery/Accra_NDVI/cloud_masked_qb10_ndvi_ge120_v02.tif")
+image_export_filename = data_dir + 'Quickbird_2010_NDVI_thresholded.npz'
+
+###############################################################################
+# Image processing code (to save image as a numpy array) starts below.
+if os.path.exists(image_export_filename):
+    raise IOError('File "%s" already exists. Manually delete file to have script regenerate it.'%image_export_filename)
+print("***Loading image data...")
+
+gt = ds.GetGeoTransform()
+image = ds.ReadAsArray()
+
+# Format of ds.GetGeoTransform is:
+# (upper left x, x-size, rotation1, upper left y, rotation2, y-size)
+# upper left is the origin for GDAL
+np.savez(image_export_filename, image=image, origin_x=gt[0], origin_y=gt[3],
+        pixel_width=gt[1], pixel_height=gt[5], cols=ds.RasterXSize,
+        rows=ds.RasterYSize)
