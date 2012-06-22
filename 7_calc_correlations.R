d038e8830f54651d41c12f14092d5d8b0a2bb478
Alex Zvoleff <azvoleff@mail.sdsu.edu>
2012-06-22 10:00:23 -0700

    Update variables for Springer chapter.

--- 7_calc_correlations.R
+++ 7_calc_correlations.R
@@ -3,33 +3,36 @@
 # Makes correlation of a WHSA variable with a neighborhood-level measure 
 # calculated over a range of different neighborhood buffer sizes.
 
-require("plotrix")
-require("ggplot2")
-require("pspearman")
+require(plotrix)
+require(ggplot2)
+require(gridExtra)
+require(reshape) # for melt
+require(pspearman)
 
 DPI <- 300
 WIDTH <- 9
 HEIGHT <- 5.67
-theme_update(theme_grey(base_size=16))
+theme_update(theme_bw(base_size=16))
+#update_geom_defaults("line", aes(size=1))
 
 # Variables to analyze with correlation analysis
 #cor_variable_names <- c("BP_diastolic", "BP_systolic", "wealth_quintile", "W401_AMOUNT_OF_PAIN")
 #cor_method_names <- c("pearson", "pearson", "spearman", "spearman")
-cor_variable_names <- c("w203_own_health")
-cor_method_names <- c("spearman")
+cor_variable_names <- c("w203_own_health", "rand_sf36_pf", "rand_sf36_ew", "rand_sf36_gh")
+cor_method_names <- c("spearman", "pearson", "pearson", "pearson")
 
 # Variables to analyze with bivariate logistic regression
 #logit_variable_names <- c("W1321A_MALARIA")
 logit_variable_names <- c()
 
-#load("~/Data/Ghana/20101206/whsa_ii_data050510.Rdata")
-load("R:/Data/Ghana/20101206/whsa_ii_data050510.Rdata")
-load("R:/Data/Ghana/20110725_From_Justin/20110727_WHSA2_SF36.Rdata")
-whsa2_15_feb_2011 <- data.frame(id=whsa2_15_feb_2011$id, w203_own_health=whsa2_15_feb_2011$w203_own_health)
-whsa2data050510 <- merge(whsa2data050510, whsa2_15_feb_2011, by.x="woman_id", by.y="id")
+#load("D:/Shared_Documents/SDSU/Ghana/AccraABM/whsa1_whsa2linked.Rdata")
+#whsa1 <- as.data.frame(whsa1_whsa2linked)
+load("D:/Shared_Documents/SDSU/Ghana/AccraABM/whsa2_spdf_FMV_ego.Rdata")
+whsa2 <- as.data.frame(whsa2_spdf_FMV_ego)
 
-min_radius <- 50
-max_radius <- 975
+data_dir <- 'R:/Data/Ghana/Egocentric_NBH_Data/20120315_25_to_1000m/'
+min_radius <- 25
+max_radius <- 1000
 dradius <- 25
 radii <- seq(min_radius, max_radius, dradius)
 
@@ -38,19 +41,15 @@ other_classes <- c(base_class, 'NONVEG')
 #col_prefix <- 'QB2002_NDVI_'
 col_prefix <- 'QB2010_NDVI_'
 
-#data_dir <- '/home/azvoleff/Data/Ghana/Egocentric_NBH_Data/'
-#data_dir <- '/home/azvoleff/Data/Ghana/Egocentric_NBH_Data/20110503_50-to-975-by-25m/'
-data_dir <- 'R:/Data/Ghana/Egocentric_NBH_Data/20110503_50-to-975-by-25m/'
 
-load(paste(data_dir, 'merged_buffer_results.Rdata', sep=""))
+load(paste(data_dir, 'merged_buffer_results_20120315.Rdata', sep=""))
 
 # First do the easy ones - correlation analysis
 for (n in 1:length(cor_variable_names)) {
     variable_name <- cor_variable_names[n]
     method_name <- cor_method_names[n]
-    variable_col <- grep(variable_name, names(whsa2data050510))
-    whsa_var <- whsa2data050510[,variable_col]
-    
+    variable_col <- grep(variable_name, names(whsa2))
+    whsa_var <- whsa2[,variable_col]
     # Save summary info to textfile
     summary_filename <- paste(data_dir, col_prefix, 'summary_', variable_name,
             '.csv', sep="")
@@ -60,7 +59,6 @@ for (n in 1:length(cor_variable_names)) {
     else if (method_name == "pearson") {
         write.csv(as.matrix(summary(whsa_var)), file=summary_filename)
     }
-
     correlations <- c()
     correlations_pvalues <- c()
     for (radius in radii) {
@@ -78,22 +76,22 @@ for (n in 1:length(cor_variable_names)) {
         correlations <- c(correlations, correlation_test$estimate)
         correlations_pvalues <- c(correlations_pvalues, correlation_test$p.value)
     }
-
     plot_title <- paste("Correlation of '", variable_name, "' with ",
             col_prefix, base_class, sep="")
-    qplot(radii, correlations, geom="line", main=plot_title,
-          xlab="Buffer Radius (meters)", ylab="Correlation")
+    corr_plot <- ggplot(data.frame(radii, correlations),aes(x=radii,y=correlations)) + 
+            geom_line() + labs(x = 'Radius (m)',y='Correlation (r)')
+    signif_plot <- ggplot(data.frame(radii, correlations_pvalues),aes(x=radii,y=correlations_pvalues)) + 
+            geom_line() + labs(x = 'Radius (m)',y='Signif. (p)')
     plot_filename <- paste(data_dir, col_prefix, 'corplot_', variable_name,
-            '.png', sep="")
-    ggsave(filename=plot_filename, width=WIDTH, height=HEIGHT, dpi=DPI)
-
-    plot_title <- paste("P-value for Correlation of '", variable_name,
-            "' with ", col_prefix, base_class, sep="")
-    qplot(radii, correlations_pvalues, geom="line", main=plot_title,
-            xlab="Buffer Radius (meters)", ylab="p-value")
+            '_combined.png', sep="")
+    png(plot_filename, width=900, height=600)
+    grid.arrange(corr_plot, signif_plot)
+    dev.off()
     plot_filename <- paste(data_dir, col_prefix, 'corplot_', variable_name,
-            '_p-values.png', sep="")
-    ggsave(filename=plot_filename, width=WIDTH, height=HEIGHT, dpi=DPI)
+            '_combined.svg', sep="")
+    svg(plot_filename, width=9, height=6)
+    grid.arrange(corr_plot, signif_plot)
+    dev.off()
 }
 
 ###############################################################################
@@ -122,8 +120,8 @@ odds_ratios <- function(logistic.model, filename=FALSE) {
 
 for (n in 1:length(logit_variable_names)) {
     variable_name <- logit_variable_names[n]
-    variable_col <- grep(variable_name, names(whsa2data050510))
-    whsa_var <- whsa2data050510[,variable_col]
+    variable_col <- grep(variable_name, names(whsa2))
+    whsa_var <- whsa2[,variable_col]
     summary_filename <- paste(data_dir, col_prefix, 'summary_', variable_name,
             '.csv', sep="")
     write.csv(table(whsa_var, useNA="always"), row.names=FALSE, file=summary_filename)
@@ -161,7 +159,7 @@ for (n in 1:length(logit_variable_names)) {
     qplot(radii, odds_ratios_pvalues, geom="line", main=plot_title,
             xlab="Buffer Radius (meters)", ylab="p-value")
     plot_filename=paste(data_dir, col_prefix, 'logitplot_', variable_name,
-            '_p-values.png',C:\Users\azvoleff\Code\R\Egocentric_NBHs sep="")
+            '_p-values.png', sep="")
     ggsave(filename=plot_filename, width=WIDTH, height=HEIGHT, dpi=DPI)
 }
 
